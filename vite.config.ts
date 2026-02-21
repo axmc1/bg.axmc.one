6c096582a336e71cdbd811ec35bdb243a08cd599
axmc <alex@axmc.one>
2026-02-21 23:05:19 +0100

    Revert "fix preview env for railway"
    This reverts commit e20b52dbff3906803679e4ef04bedeee1da62462.

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    host: true,
-    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
-  },
 })
